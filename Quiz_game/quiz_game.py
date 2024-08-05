35ab4c982d99a70fe6f1f51aea430999af2f5fb5
Dev-Netsanet <Netsanetsolomonmulu@gmail.com>
2024-08-06 00:32:38 +0300

    finished

--- Quiz_game/quiz_game.py
+++ Quiz_game/quiz_game.py
@@ -5,9 +5,19 @@ if playing.lower() != "y":
     quit()
 print("Okay! Let's Play")
 score = 0
-answer:str = input("What is CPU").lower()
+answer:str = input("What is CPU stands for? ").lower()
 if answer == "central processing unit":
     print("Correct!")
     score +=1
 else:
     print("Incorrect!")
+
+answer:str = input("What is GPU stands for? ").lower()
+if answer == "graphics processing unit": 
+    print("Correct!")
+    score +=1
+else:
+    print("Incorrect!")
+
+print(f"Your score is {score}")
+print(f"your score percent is {(score*score)//100}%")
